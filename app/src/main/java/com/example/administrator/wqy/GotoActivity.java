3f8ca79de2dc5a41cc42668ff2a9dd43654fa28b
Administrator <y9791994@163.com>
2016-09-21 22:06:45 +0800

    test

---
+++ app/src/main/java/com/example/administrator/wqy/GotoActivity.java
@@ -0,0 +1,14 @@
+package com.example.administrator.wqy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nine);
+    }
+}
--- app/src/main/java/com/example/administrator/wqy/MainActivity.java
+++ app/src/main/java/com/example/administrator/wqy/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.administrator.wqy;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    Button loginBt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.oo);
+        loginBt = (Button) findViewById(R.id.btId);
+        gotoAnotherActivity();
+    }
+    public void gotoAnotherActivity(){
+        loginBt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this,GotoActivity.class);
+                startActivity(intent);
+                Toast.makeText(getApplicationContext(),"跳转到已登陆界面", Toast.LENGTH_SHORT).show();
+            }
+        });
+        loginBt.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(getApplicationContext(),"sssssk",Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+
+       /* loginBt.setOnLongClickListener(new View.OnLongClickListener(){
+            @Override
+            public boolean onLongClick(View v){
+                Toast.makeText(getApplicationContext(),"长按了登陆按钮",Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });*/
     }
 }
